cbc3e9d17058f035eb890552e040f521c9a7e870
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2020-11-29 18:37:06 +0100

    run pre-commit

--- src/configsyncop.py
+++ src/configsyncop.py
@@ -1,10 +1,10 @@
-import json
 import hashlib
+import json
 import logging
 
 import kopf
-import pykube
 import prometheus_client as prometheus
+import pykube
 
 logger = logging.getLogger(__name__)
 
@@ -15,22 +15,23 @@ HASH_ANNOTATION = f"{BASE_ANNOTATION}.hash"
 prometheus.start_http_server(9090)
 
 REQUEST_TIME = prometheus.Summary(
-    'event_processing_seconds',
-    'Time spent processing events',
+    "event_processing_seconds",
+    "Time spent processing events",
     ["resource"],
 )
 
 CONFIG_CHANGE = prometheus.Counter(
-    'config_change',
-    'Number of configuration changes',
+    "config_change",
+    "Number of configuration changes",
     ["namespace", "resource", "name"],
 )
 
 RESOURCE_PATCH = prometheus.Counter(
-    'resource_patch',
-    'patches by resource',
+    "resource_patch",
+    "patches by resource",
     ["namespace", "resource", "name"],
 )
+
 
 class ConfigResource:
     kind = "unknown"
@@ -41,7 +42,7 @@ class ConfigResource:
         self.name = name
         self.annotations = annotations
         self._kwargs = kwargs
-        self._hash = self._generate_hash(body.get('data'))
+        self._hash = self._generate_hash(body.get("data"))
         self._annotation = f"{HASH_ANNOTATION}/{self.kind.lower()}-{self.name}"
         CONFIG_CHANGE.labels(namespace, self.kind, name).inc()
 
@@ -50,7 +51,9 @@ class ConfigResource:
             if not self._must_handle():
                 logger.debug(f"{self.kind}/{self.name} not annotated")
                 return
-            logger.info(f"Handler for {self.kind} {self.name} on {self.namespace} -> {self._hash}")
+            logger.info(
+                f"Handler for {self.kind} {self.name} on {self.namespace} -> {self._hash}"
+            )
             self._update_template_based_resource(pykube.Deployment, "Deployment")
             self._update_template_based_resource(pykube.DaemonSet, "DaemonSet")
             self._update_template_based_resource(pykube.StatefulSet, "StatefulSet")
@@ -78,20 +81,25 @@ class ConfigResource:
         api = pykube.HTTPClient(pykube.KubeConfig.from_env())
         logger.debug(f"Retrieving {res_name} resources")
         for resource in PykubeResource.objects(api).filter(namespace=self.namespace):
-            if not self.is_used_by_spec(resource.obj['spec']['template']['spec']):
-                logger.debug(f"{res_name} {resource.name} not used by {self.kind}/{self.name}")
+            if not self.is_used_by_spec(resource.obj["spec"]["template"]["spec"]):
+                logger.debug(
+                    f"{res_name} {resource.name} not used by {self.kind}/{self.name}"
+                )
                 continue
             if resource.annotations.get(self._annotation) == self._hash:
-                logger.debug(f"{res_name} {resource.name} is up-to-date: {annotation} == {self._hash}")
+                logger.debug(
+                    f"{res_name} {resource.name} is up-to-date:"
+                    f" {self._annotation} == {self._hash}"
+                )
                 continue
             yield resource
 
     def _update_template_based_resource(self, PykubeResource, res_name):
         patch = {
-            'spec': {
-                'template': {
-                    'metadata': {
-                        'annotations': {
+            "spec": {
+                "template": {
+                    "metadata": {
+                        "annotations": {
                             self._annotation: self._hash,
                         }
                     }
@@ -104,11 +112,11 @@ class ConfigResource:
             RESOURCE_PATCH.labels(self.namespace, res_name, resource.name).inc()
 
     def is_used_by_spec(self, spec):
-        for container in spec['containers']:
-            for var in container.get('env', []):
+        for container in spec["containers"]:
+            for var in container.get("env", []):
                 if self.is_used_by_var(var):
                     return True
-        for volume in spec.get('volumes', []):
+        for volume in spec.get("volumes", []):
             if self.is_used_by_volume(volume):
                 return True
         return False
@@ -128,24 +136,26 @@ class Secret(ConfigResource):
         super().__init__(**kwargs)
 
     def is_used_by_var(self, var):
-        return var.get('valueFrom', {}).get('secretKeyRef', {}).get('name') == self.name
+        return var.get("valueFrom", {}).get("secretKeyRef", {}).get("name") == self.name
 
     def is_used_by_volume(self, volume):
-        return volume.get('secret', {}).get('secretName') == self.name
+        return volume.get("secret", {}).get("secretName") == self.name
 
 
 class ConfigMap(ConfigResource):
     kind = "ConfigMap"
-    REF = ["","v1", "configmaps"]
+    REF = ["", "v1", "configmaps"]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def is_used_by_var(self, var):
-        return var.get('valueFrom', {}).get('configMapKeyRef', {}).get('name') == self.name
+        return (
+            var.get("valueFrom", {}).get("configMapKeyRef", {}).get("name") == self.name
+        )
 
     def is_used_by_volume(self, volume):
-        return volume.get('configMap', {}).get('name') == self.name
+        return volume.get("configMap", {}).get("name") == self.name
 
 
 @kopf.on.update(*(Secret.REF))
